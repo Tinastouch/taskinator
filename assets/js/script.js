8d936bf0ea200f64b7e136fca845124b6d74cd21
Tinastouch <tinastouch@gmail.com>
2021-12-13 11:12:36 -0500

    Handle Form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,15 @@
-var buttonEl = document.querySelector("#save-task"); 
+var formEl = document.querySelector("#task-form");
 
-var createTaskHandler = function() { 
-  var tasksToDoEl = document.querySelector("#tasks-to-do"); 
-  var listItemEl = document.createElement("li"); 
-  listItemEl.className = "task-item"; 
-  listItemEl.textContent = "This is a new task."; 
-  tasksToDoEl.appendChild(listItemEl); 
-  }; 
+var createTaskHandler = function(event) {
+    console.log(event);
 
-buttonEl.addEventListener("click", createTaskHandler);
+    event.preventDefault();
+    var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+  };
+
+  formEl.addEventListener("submit", createTaskHandler);
